a3e107d78a7489c9426dd1247d80581c9583d681
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2021-07-28 21:34:55 +0300

    delete frontend directory

--- frontend/src/components/ErrorMessagePopup.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Popup from "./Popup";
-
-export default function ErrorMessagePopup({ errorMessage }) {
-  return (
-    <Popup name="error-message" isOpen={errorMessage} title={errorMessage} />
-  );
-}
